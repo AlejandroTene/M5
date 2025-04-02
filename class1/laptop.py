3a5cdd4f32b6355722b5d9488b7abf5ba314fcea
AlejandroTene <carlostene2006@gmail.com>
2025-04-02 12:19:36 -0500

    Reto 8

---
+++ class1/laptop.py
@@ -0,0 +1,45 @@
+import random
+class lapto:
+    def __init__(self, marca, procesador, memoria, costo=500, impuestos=10):
+        self.marca=marca
+        self.procesador=procesador
+        self.memoria=memoria
+        self.costo=costo
+        self.impuestos=impuestos
+    
+    def valor_final(self):
+        return self.costo+self.impuestos
+    
+    def valor_descuento(self,descuento):
+        return (self.costo*descuento)/100
+    
+    def realizar_diagnostico_sistema(self):
+        resultado={
+            "MARCA": f"{self.marca}",
+            "PROCESADOR": f"{self.procesador}",
+            "MEMORIA": "OK" if self.memoria >= 8 else "Aumentar memoria RAM",
+            "BATERIA":"OK" if random.choice([True,False]) else "Cambiar de bateria"
+        }
+        return resultado
+    
+    
+
+
+    
+    @staticmethod
+    def comparar_costo(Laptop1, Laptop2):
+        if Laptop1.costo == Laptop2.costo:
+            return "Los costos son igules"
+        else:
+            return "Los costos son diferentes"
+
+    @classmethod
+    def asus_laptop(cls, costo):
+        marca = "asus"
+        procesador = "i5"
+        memoria = 16
+        return cls(marca,procesador,memoria,costo)
+
+
+
+
---
+++ class1/laptop_business.py
@@ -0,0 +1,24 @@
+from laptop import lapto
+import random
+
+class Laptop_Business(lapto):
+    def __init__(self, marca, procesador, memoria, almacenamiento, duracion_bateria, costo=500, impuestos=10):
+        super().__init__(marca, procesador, memoria, costo, impuestos)
+        self.almacenamiento = almacenamiento
+        self. duracion_bateria = duracion_bateria
+
+    def realizar_diagnostico_sistema(self):
+        resultado_diagnostico = super().realizar_diagnostico_sistema()
+        resultado_conectividad = self.verificar_conectividad()
+        resultado_diagnostico["Resultado conectividad"] = resultado_conectividad
+        return resultado_diagnostico
+
+    def verificar_conectividad(self):
+        datos = {
+            "WIFI": "True" if random.choice([True, False]) else "False",
+            "Acceso_Servidores": "False" if random.choice([True, False]) else "False",
+            "Latencia_baja": "True" if random.choice([True, False]) else "False"
+        }
+        return datos
+
+    pass
